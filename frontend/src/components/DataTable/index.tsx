87b22797d6d131d2211f19eb5cfcef5014668dfa
DemissonAndrade <demissondsa@gmail.com>
2021-11-01 19:37:16 -0300

    Basic static components

---
+++ frontend/src/components/DataTable/index.tsx
@@ -0,0 +1,92 @@
+const DataTable = () => {
+
+    return (
+        <div className="table-responsive">
+        <table className="table table-striped table-sm">
+            <thead>
+                <tr>
+                    <th>Data</th>
+                    <th>Vendedor</th>
+                    <th>Clientes visitados</th>
+                    <th>Negócios fechados</th>
+                    <th>Valor</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>22/04/2021</td>
+                    <td>Naruto</td>
+                    <td>34</td>
+                    <td>25</td>
+                    <td>15017.00</td>
+                </tr>
+                <tr>
+                    <td>22/04/2021</td>
+                    <td>Goku</td>
+                    <td>34</td>
+                    <td>25</td>
+                    <td>15017.00</td>
+                </tr>
+                <tr>
+                    <td>22/04/2021</td>
+                    <td>Vedita</td>
+                    <td>34</td>
+                    <td>25</td>
+                    <td>15017.00</td>
+                </tr>
+                <tr>
+                    <td>22/04/2021</td>
+                    <td>Michael kyle</td>
+                    <td>34</td>
+                    <td>25</td>
+                    <td>15017.00</td>
+                </tr>
+                <tr>
+                    <td>22/04/2021</td>
+                    <td>Bart Simpson</td>
+                    <td>34</td>
+                    <td>25</td>
+                    <td>15017.00</td>
+                </tr>
+                <tr>
+                    <td>22/04/2021</td>
+                    <td>Andrade</td>
+                    <td>34</td>
+                    <td>25</td>
+                    <td>15017.00</td>
+                </tr>
+                <tr>
+                    <td>22/04/2021</td>
+                    <td>Demisson</td>
+                    <td>34</td>
+                    <td>25</td>
+                    <td>15017.00</td>
+                </tr>
+                <tr>
+                    <td>22/04/2021</td>
+                    <td>Kenny</td>
+                    <td>34</td>
+                    <td>25</td>
+                    <td>15017.00</td>
+                </tr>
+                <tr>
+                    <td>22/04/2021</td>
+                    <td>Wolverine</td>
+                    <td>34</td>
+                    <td>25</td>
+                    <td>15017.00</td>
+                </tr>
+                <tr>
+                    <td>22/04/2021</td>
+                    <td>Donatelo</td>
+                    <td>34</td>
+                    <td>25</td>
+                    <td>15017.00</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    );
+}
+
+export default DataTable;
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/DemissonAndrade" target="_blank" rel="noreferrer">DemissonAndrade</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/DemissonAndrade.ig" target="_blank" rel="noreferrer">@DemissonAndrade.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
