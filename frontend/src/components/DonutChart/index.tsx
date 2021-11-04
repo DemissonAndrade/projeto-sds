864c74672f22d9dd842bc6b62ce52c6c4a846c3d
DemissonAndrade <demissondsa@gmail.com>
2021-11-03 21:36:57 -0300

    Security config

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -5,7 +5,7 @@ const DonutChart = () => {
 
     const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+        labels: ['João', 'Josef', 'Kal-El', 'ciclope', 'Neymar']
     }
     
     const options = {
